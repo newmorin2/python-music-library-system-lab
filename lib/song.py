ddc76191824fcc9d76653ae3bb032bf138063415
newmorin2 <newton.mwangi@student.moringaschool.com>
2026-06-09 15:39:13 +0300

    class

--- lib/song.py
+++ lib/song.py
@@ -1,2 +1,29 @@
-class Song:
-    pass
+class Song():
+    count = 0
+    genres = []
+    artists = []
+    genre_count = {}
+    artists_count = {}
+
+    def __init__(self, name, artist, genre):
+        self.name = name
+        self.artist = artist
+        self.genre = genre
+
+        Song.count += 1
+
+        if artist not in Song.artists:
+            Song.artists.append(artist)
+
+        if genre not in Song.genres:
+            Song.genres.append(genre)
+
+        if genre in Song.genre_count:
+            Song.genre_count[genre] += 1
+        else:
+            Song.genre_count[genre] = 1
+
+        if artist in Song.artists_count:
+            Song.artists_count[artist] += 1
+        else:
+            Song.artists_count[artist] = 1
